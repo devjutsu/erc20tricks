4e429566ea84eb43309f643ea4b8314831b3ce89
Albert Garuda <algarud@gmail.com>
2022-07-11 22:47:42 +0300

    dummic

---
+++ Dummic.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Dummic is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("Dummic", "DUM") {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount * 10 ** decimals());
+    }
+}
