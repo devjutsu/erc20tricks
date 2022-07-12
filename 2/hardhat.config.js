6ea7a2df720b7fefdfbeb4fc5774e02b538dbf2c
Albert Garuda <algarud@gmail.com>
2022-07-12 22:43:13 +0300

    more deps

--- 2/hardhat.config.js
+++ 2/hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.15",
+  networks: {
+    rinkeby: {
+      url: process.env.INFURA_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    polygon: {
+      url: 'https://polygon-rpc.com',
+      accounts:[process.env.PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
