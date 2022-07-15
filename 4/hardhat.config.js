f9aaeefa3862682e0a0e85cfb72d75ddab62fb9f
Albert Garuda <algarud@gmail.com>
2022-07-15 14:01:51 +0300

    4 devenv, hardhat conifg

--- 4/hardhat.config.js
+++ 4/hardhat.config.js
@@ -1,6 +1,22 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.15",
+  networks: {
+    rinkeby: {
+      url: 'https://rinkeby.infura.io/v3/749c7c4e8f6543cf85b40be1f5fe4e72',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    polygon: {
+      url: 'https://polygon-rpc.com',
+      accounts:[process.env.PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
